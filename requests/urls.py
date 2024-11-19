82c68ce6ded38f323853bf935f87b862528e1664
RebellWrynn <egzinivan@gmail.com>
2024-11-19 13:31:49 +0500

    create def in views

--- requests/urls.py
+++ requests/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.authorization,name="authorization"),
     path('',views.user_requests,name="requests")
 ]
--- requests/views.py
+++ requests/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
+
+def authorization(request):
+    return TemplateResponse(
+        request, "authorization.html"
+    )
 
 def user_requests(request):
-    return 
+    return TemplateResponse(
+        request, "requests.html"
+    )
 # Create your views here.
