11d0f01a8c994680b55148b1daa5f2c4b10578b3
RebellWrynn <egzinivan@gmail.com>
2024-11-24 18:51:00 +0500

    add admin staff and modeles

--- requests/admin.py
+++ requests/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Employees,User_Requests
+
+admin.site.register(Employees)
+admin.site.register(User_Requests)
+
 # Register your models here.
--- requests/models.py
+++ requests/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
+class Employees(models.Model):
+    Employees_id = models.CharField(primary_key='True',max_length=20)
+    Firstname = models.CharField('Firstname',max_length=30)
+    Lastname = models.CharField('Lastname',max_length=30)
+    Surname = models.CharField('Surname',max_length=30)
+
+
+class User_Requests(models.Model):
+    Employees_id = models.ForeignKey(Employees,on_delete=models.CASCADE)
+    Request_id = models.CharField(primary_key='True',max_length=20)
+    Them = models.CharField(max_length=30)
+    Requesttype = models.CharField(max_length=30)
+    Priority = models.CharField(max_length=30)
+    Description = models.CharField(max_length=30)
+    Date = models.DateField()
+    Responsibleemployee = models.CharField(max_length=30)
 # Create your models here.
